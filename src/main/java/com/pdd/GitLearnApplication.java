973bf37e75fa6503af368dd0b66272a9ed2d046f
pdd <876085987@qq.com>
2021-03-02 15:58:49 +0800

    first time commit

---
+++ src/main/java/com/pdd/GitLearnApplication.java
@@ -0,0 +1,13 @@
+package com.pdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLearnApplication.class, args);
+    }
+
+}
